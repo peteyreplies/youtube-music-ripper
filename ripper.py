babb4d1f0362659b1a73d05e7f23971f13c082e4
Chris Peterson <chris@cpeterson.org>
2014-02-16 01:45:34 -0500

    naother commit

--- ripper.py
+++ ripper.py
@@ -2,16 +2,20 @@
 #youtube-music-ripper 
 #by @peteyreplies 
 #NOTE: REQUIRES YOUTUBE-DL TO WORK - http://rg3.github.io/youtube-dl/download.html
-#youtube-dl -citw ytuser:LaBelleChannel --extract-audio --audio-format mp3
 ###
 
+#import stuff
+import youtube-dl
+
+
 #prompt them for choice of download
-print "What do you want to download? Enter 1, 2, or 3 at the prompt"
+print "What do you want to download? Enter 1, 2, 3, or 4 at the prompt"
 print "1) an individual video"
 print "2) all videos by a user/channel"
 print "3) all videos in a playlist"
+print "4) a txt file of youtube links"
 
-choice = input("Enter 1, 2, or 3 here:")
+choice = input("Enter 1, 2, 3, or 4 here:")
 
 #validate the choice 
 if int(input) 
