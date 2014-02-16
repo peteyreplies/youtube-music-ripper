38f3d8399ffa99f7da5244395439e9b9ed1feccf
Chris Peterson <chris@cpeterson.org>
2014-02-16 18:47:04 -0500

    added metadata flags and m4a cleaning

--- ripper.py
+++ ripper.py
@@ -15,8 +15,10 @@ print ""
 print "What do you want to rip? Enter 1, 2, 3, or 4 at the prompt!"
 print "\t 1: audio from an individual video"
 print "\t 2: audio from a list of video links in a .txt file in this folder"
-print "\t 3: audio from all videos by a user/channel"
+print "\t 3: audio from all videos by a YouTube user/channel"
 print "\t 4: audio from all videos in a playlist"
+print ""
+print "***WARNING: THIS SCRIPT WILL ERASE *ALL* .M4A FILES IN THIS DIRECTORY AFTER RUNNING***"
 print ""
 
 choice = input("Enter 1, 2, 3, or 4 here: ")
@@ -24,25 +26,29 @@ choice = input("Enter 1, 2, 3, or 4 here: ")
 if choice == 1: 
 	print "Paste the link to the video in the prompt: "
 	link = raw_input()
-	subprocess.call("youtube-dl " + link + " -itw --extract-audio --audio-format mp3", shell=True)
+	subprocess.call("youtube-dl " + link + " -itw --extract-audio --audio-format mp3 --audio-quality 0 --add-metadata", shell=True)
+	subprocess.call("rm *.m4a", shell=True)
 	print "Ripped!"
 
 if choice == 2: 
 	print "Enter the name of the text file (e.g. links.txt): "
 	links = raw_input()
-	subprocess.call("youtube-dl -itw --extract-audio --audio-format mp3 -a, --batch-file " + links, shell=True)
+	subprocess.call("youtube-dl -itw --extract-audio --audio-format mp3 --audio-quality 0 --add-metadata -a, --batch-file " + links, shell=True)
+	subprocess.call("rm *.m4a", shell=True)
 	print "Ripped!"
 
 if choice == 3: 
 	print "Paste *only* the user/channel name into the prompt: "
 	user = raw_input()
-	subprocess.call("youtube-dl -itw ytuser:" + user + " --extract-audio --audio-format mp3", shell=True)
+	subprocess.call("youtube-dl -itw ytuser:" + user + " --extract-audio --audio-format mp3 --audio-quality 0 --add-metadata", shell=True)
+	subprocess.call("rm *.m4a", shell=True)
 	print "Ripped!"
 
 if choice == 4: 
 	print "Paste a link to the playlist (it should have the word '''playlist''' in the URL) into the prompt: " 
 	playlist = raw_input()
-	subprocess.call("youtube-dl " + playlist + " -itw --extract-audio --audio-format mp3", shell=True)
+	subprocess.call("youtube-dl " + playlist + " -itw --extract-audio --audio-format mp3 --audio-quality 0 --add-metadata", shell=True)
+	subprocess.call("rm *.m4a", shell=True)
 	print "Ripped!"
 
 if choice not in range (1,4): 
