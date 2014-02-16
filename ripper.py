2e677723b80ecaf80e97256c3151f974502e7096
Chris Peterson <chris@cpeterson.org>
2014-02-16 10:56:46 -0500

    added some validation code which may not work

--- ripper.py
+++ ripper.py
@@ -10,12 +10,18 @@ import youtube-dl
 
 #prompt them for choice of download
 print "What do you want to download? Enter 1, 2, 3, or 4 at the prompt"
-print "1) an individual video"
-print "2) all videos by a user/channel"
-print "3) all videos in a playlist"
-print "4) a txt file of youtube links"
+print "\t 1) an individual video"
+print "\t 2) a list of individual video links in a .txt file"
+print "\t 3) all videos by a user/channel"
+print "\t 4) all videos in a playlist"
+print ""
 
 choice = input("Enter 1, 2, 3, or 4 here:")
 
 #validate the choice 
-if int(input) 
+if 1 >= int(input) <= 4: 
+	pass
+else:
+	print "You entered an invalid number! Why do you hate me and/or yourself?"
+	break
+
